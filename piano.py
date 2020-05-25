857c9ba913b27d21abb1030d36d895dd5eb17434
Emily J <Emily@Emilys-MacBook-Pro-89.local>
2020-05-25 14:22:39 -0400

    code works

--- piano.py
+++ piano.py
@@ -2,6 +2,11 @@ from bs4 import BeautifulSoup
 from requests import get
 from collections import Counter
 import re
+import json
+from dotenv import load_dotenv
+import spotipy
+import spotipy.util as util
+load_dotenv()
 
 class Piano:
     keyboard = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
@@ -32,7 +37,7 @@ class Piano:
 
     ##
     def clean_chord(self, chord):
-        minor = ("m" in chord)
+        minor = ("m" in chord) and ("maj" not in chord)
         if len(chord) > 1 and chord[1] in ["b", "#"]:
             chord = chord[:2]
         else:
@@ -72,7 +77,8 @@ class Piano:
         halfsteps = (self.keyboard.index(high) - self.keyboard.index(low)) % len(self.keyboard)
         return self.halfstep_to_interval[halfsteps]
 
-    def get_chord_numbers(self, chords, key):
+    def get_chord_numbers(self, chords):
+        key = self.get_major_key_from_chords(chords)
         chord_numbers = []
         for chord in chords:
             chord, minor = self.clean_chord(chord)
@@ -83,18 +89,133 @@ class Piano:
             chord_numbers.append(chord_number)
         return chord_numbers
 
-class Ukutabs():
+    def get_pattern(self, chord_numbers):
+        best_pattern_freq = 0
+        best_pattern = None
+        best_pattern_length = 0
+        for pattern_length in range(2, 7):
+            # looks at chord progressions of length pattern_length
+            lines = [chord_numbers[i: i+pattern_length] for i in range(len(chord_numbers) - pattern_length)]
+            filtered_lines = []
+            # sort each chord progression and keep if distinct
+            for line in lines:
+                line.sort()
+                distinct = True
+                for j in range(len(line) - 1):
+                    distinct = distinct and (line[j] != line[j+1])
+                if distinct:
+                    filtered_lines.append("/".join(line))
+            # note frequency of most common chord progression
+            if len(filtered_lines) > 0:
+                freqs = Counter(filtered_lines)
+                pattern, freq = freqs.most_common(1)[0]
+                if freq > best_pattern_freq:
+                    best_pattern_freq = freq
+                    best_pattern = freqs
+                    best_pattern_length = pattern_length
+        return (best_pattern, best_pattern_length)
+
+class Ukutabs:
     def __init__(self):
         pass
 
-    def get_chords(self, url):
+    def get_chords_from_url(self, url):
         response = get(url)
         soup = BeautifulSoup(response.text, features = "lxml")
         chords = [x["name"] for x in soup.select(".hoverchord")]
         return chords
 
-class UltimateGuitar():
+    def get_chords_from_song_info(self, artist, song_name):
+        artist = artist.replace(" ", "-")
+        # remove anything after "(" or "-"
+        song_name = song_name.split(" (")[0]
+        song_name = song_name.split(" -")[0]
+        # remove punctuation
+        song_name = song_name.replace("’", "")
+        url = "https://ukutabs.com/" + artist[0] + "/" + artist + "/" + song_name + "/"
+        chords = ukutabs.get_chords(url)
+        return chords
+
+class UltimateGuitar:
     def __init__(self):
         pass
 
+    def get_chords_from_url(self, url):
+        response = get(url)
+        soup = BeautifulSoup(response.text, features = "lxml")
+        chords = str(soup).split("[tab]", 1)[1].split("[ch]")
+        clean_chords = []
+        for i, chord in enumerate(chords):
+            if "[/ch]" in chord:
+                clean_chords.append(chord.split("[/ch]")[0])
+        return clean_chords
+
+    def get_chords_from_song_info(self, artist, song_name):
+        found = False
+        # remove anything after "(" or "-"
+        song_name = song_name.split(" (")[0]
+        song_name = song_name.split(" -")[0]
+        # remove punctuation
+        song_name = song_name.replace("’", "")
+        url = "https://www.ultimate-guitar.com/search.php?search_type=title&value="
+        for word in artist.split(" ") + song_name.split(" "):
+            url += word
+            url += "%20"
+        response = get(url)
+        soup = BeautifulSoup(response.text, features = "lxml")
+        chords_div = soup.find_all("div", {"class": "js-store"})[0]
+        d= json.loads(chords_div.get("data-content"))
+        for song in d["store"]["page"]["data"]["results"]:
+            song_url = song["tab_url"]
+            if song_name.lower() in song["song_name"].lower() and "/pro/" not in song_url:
+                return self.get_chords_from_url(song_url)
+
+
+
+class Playlist:
+    def __init__(self, username, playlist_name):
+        self.username = username
+        # assumes that client_id, client_secret, redirect_uri are in OS environment
+        self.token = util.prompt_for_user_token(username)
+        self.sp = spotipy.Spotify(auth = self.token)
+        self.piano = Piano()
+        self.ug = UltimateGuitar()
+        self.playlist_name = playlist_name
+        self.playlist_id = self.get_playlist_id()
+
+    def get_playlist_id(self):
+        playlists = self.sp.user_playlists(self.username)['items']
+        for playlist in playlists:
+            if playlist["name"].lower() == self.playlist_name.lower():
+                return playlist["id"]
+        raise ValueError('playlist not found')
+
+    def get_playlist_songs(self):
+        results = self.sp.playlist(self.playlist_id, fields="tracks,next")
+        tracks = results['tracks']
+        artists = []
+        songs = []
+        for item in tracks['items']:
+            track = item['track']
+            artists.append(track['artists'][0]['name'])
+            songs.append(track['name'])
+        return list(zip(artists, songs))
+
+    def get_playlist_chords(self):
+        playlist_songs = self.get_playlist_songs()
+        playlist_chords = []
+        playlist_titles = []
+        for artist, title in playlist_songs:
+            chords = self.ug.get_chords_from_song_info(artist, title)
+            if chords:
+                playlist_chords.append(chords)
+                playlist_titles.append(title)
+        return playlist_titles, playlist_chords
+
+    def get_playlist_patterns(self):
+        playlist_titles, playlist_chords = self.get_playlist_chords()
+        chord_numbers = list(map(self.piano.get_chord_numbers, playlist_chords))
+        patterns = list(map(self.piano.get_pattern, chord_numbers))
+        most_common_pattern = list(map(lambda x: x[0].most_common(1)[0][0], patterns))
+        return playlist_titles, most_common_pattern
 
